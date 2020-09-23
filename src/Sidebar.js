8f4d7c7eaa4b0edcc2d790178c75b2321aea8736
HtetOoNaing <htetoonaing.ycc@gmail.com>
2020-09-23 22:53:29 +0630

    add content api with reducer

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -8,14 +8,13 @@ import ChatIcon from "@material-ui/icons/Chat";
 import StorefrontIcon from "@material-ui/icons/Storefront";
 import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
 import { ExpandMoreOutlined } from "@material-ui/icons";
+import { useStateValue } from "./StateProvider";
 
 function Sidebar() {
+  const [{ user }, dispatch] = useStateValue();
   return (
     <div className="sidebar">
-      <SidebarRow
-        src="https://lh3.googleusercontent.com/ogw/ADGmqu9vdmGLZALKQgOI-yRWGuO5CUGvZiXRTWkvU0El=s32-c-mo"
-        title="Htet Oo Naing"
-      />
+      <SidebarRow src={user.photoURL} title={user.displayName} />
       <SidebarRow
         Icon={LocalHospitalIcon}
         title="COVID-19 Information Center"
