8d4ec86e2b527b935d73e5610cdb8035fb9ec11d
HtetOoNaing <htetoonaing.ycc@gmail.com>
2020-09-23 21:04:54 +0630

    add google auth and context api

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
 const firebaseConfig = {
   apiKey: "AIzaSyBWR7wJufQlVFmD4wgbeq5aex-XeDyIgew",
   authDomain: "facebook-clone-7987e.firebaseapp.com",
@@ -9,3 +10,11 @@ const firebaseConfig = {
   appId: "1:746645291212:web:50b9ef6148bfa315358269",
   measurementId: "G-3C9PGGQSMX",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
