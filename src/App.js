33e96e5442298e1d2b270f2bc06958aaebb96d12
HtetOoNaing <htetoonaing.ycc@gmail.com>
2020-09-23 09:12:32 +0630

    finish widget

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import "./App.css";
 import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Widgets from "./Widgets";
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
-      {/* Header */}
       <Header />
-      {/* App Body */}
       <div className="app__body">
-        {/* Sidebar */}
         <Sidebar />
-        {/* Feed */}
         <Feed />
-        {/* Widgets */}
+        <Widgets />
       </div>
     </div>
   );
